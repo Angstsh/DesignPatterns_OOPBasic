3db8028146a0769a92b9e50b3ab09b4b3795beae
Angstsh <1305065108@qq.com>
2020-10-20 17:35:43 +0800

    OOP基础案例v2.1 添加带参构造方法，重载无参构造方法，使得猫实例化时可以有名或无名

--- ObjectOriented/Cat.cs
+++ ObjectOriented/Cat.cs
@@ -8,9 +8,21 @@ namespace ObjectOriented
 {
     class Cat
     {
+        private string name = "";
+        //创建带name参数的构造函数，让小猫在实例化时便可拥有名字
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+        //方法重载，允许实例化时没取名字
+        public Cat()
+        {
+            this.name = "无名";
+        }
+        
         public string Shout()
         {
-            return "喵";
+            return "我的名字叫"+name+" 喵";
         }
     }
 }
